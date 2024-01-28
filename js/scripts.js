a10a4a83ddfa440b676af1a2f17b4c1ecd358ee4
Lucas Silva <lucas.cabr.silva40@gmail.com>
2024-01-28 19:07:12 -0300

    arrumando hortografia

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,10 @@ const iconClose = document.querySelector(".icon-close");
 iconClose.addEventListener("click", () => {
   popupContato.classList.remove("active");
 });
+
+
+// ALERT NAO FUNCIONA AINDA
+
+const btnCv = document.querySelector(".btn-CV-download").addEventListener('click', () =>{
+  alert('Ainda não está funcionado o Download')
+})
